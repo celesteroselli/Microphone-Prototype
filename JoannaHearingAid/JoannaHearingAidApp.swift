001d3e7fec3169a7cd308a3cd0915a27d57ecca5
Celeste Roselli <celesteroselli@icloud.com>
2023-12-25 08:52:18 -0500

    Initial Commit

---
+++ JoannaHearingAid/JoannaHearingAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoannaHearingAidApp.swift
+//  JoannaHearingAid
+//
+//  Created by Celeste Roselli on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct JoannaHearingAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JoannaHearingAidUITests/JoannaHearingAidUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JoannaHearingAidUITestsLaunchTests.swift
+//  JoannaHearingAidUITests
+//
+//  Created by Celeste Roselli on 12/25/23.
+//
+
+import XCTest
+
+final class JoannaHearingAidUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
